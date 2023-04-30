efdacfcb0a52b8f4f9b9e4be30c1d3ad67107e74
sanuliweerasinghe <sanuliweerasinghe@gmail.com>
2023-04-30 13:57:24 +0530

    Update Login
    Updated the Login method with Spring Security's AuthenticationManager to authenticate the user

--- FlightReservationSystem/src/main/java/com/flightreservationsystem/Controllers/UsersController.java
+++ FlightReservationSystem/src/main/java/com/flightreservationsystem/Controllers/UsersController.java
@@ -2,12 +2,18 @@ package com.flightreservationsystem.Controllers;
 
 import com.flightreservationsystem.Models.Users;
 import com.flightreservationsystem.Repositories.IUsersRepository
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import jakarta.servlet.http.HttpSession;
 
 //Marking class as Controller
 @Controller
@@ -32,6 +38,9 @@ public class UsersController
 
         return "LoginForm";
     }
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("/LoginForm")
     public String login(@ModelAttribute("login") Users login, BindingResult bindingResult, Model model, HttpSession session) {
@@ -60,19 +69,32 @@ public class UsersController
         // When the user exists and the password is correct
         else
         {
-            // Setting the username in the session to be accessed later in the application
-            session.setAttribute("username", user.getUsername());
-
-            // For the admin login, redirects to the Admin Dashboard
-            if (user.getUsername().equals("Admin"))
+            try
             {
-                return "../static/AdminDashboard";
+                // Authenticates the user's credentials using the provided authentication manager.
+                Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+                // Setting the authentication object in the context holder for security.
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+                // Setting the username in the session to be accessed later in the application
+                session.setAttribute("username", user.getUsername());
+
+                // For the admin login, redirects to the Admin Dashboard
+                if (user.getUsername().equals("Admin"))
+                {
+                    return "../static/AdminDashboard";
+                }
+
+                // For the user login, redirects to the User Dashboard
+                else
+                {
+                    return "../static/UserDashboard";
+                }
             }
-
-            // For the user login directed to User dashboard
-            else
+            catch (Exception e)
             {
-                return "../static/UserDashboard";
+                return "LoginForm";
             }
         }
     }
--- FlightReservationSystem/src/main/java/com/flightreservationsystem/FlightReservationSystemApplication.java
+++ FlightReservationSystem/src/main/java/com/flightreservationsystem/FlightReservationSystemApplication.java
@@ -2,8 +2,10 @@ package com.flightreservationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class FlightReservationSystemApplication
 {
     public static void main(String[] args)
