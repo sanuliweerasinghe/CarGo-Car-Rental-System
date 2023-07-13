435f316202fd03f3851011f1ceea23f48a579de5
sanuliweerasinghe <sanuliweerasinghe@gmail.com>
2023-07-13 20:22:50 +0530

    Update ReservationsController.java

--- FlightReservationSystem/src/main/java/com/flightreservationsystem/Controllers/ReservationsController.java
+++ FlightReservationSystem/src/main/java/com/flightreservationsystem/Controllers/ReservationsController.java
@@ -3,8 +3,8 @@ package com.flightreservationsystem.Controllers;
 import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
-import javax.servlet.http.HttpSession;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
