ae7931dcb3be74150ebb175dcae3e03ea9bcb97d
sanuliweerasinghe <sanuliweerasinghe@gmail.com>
2023-07-13 20:22:33 +0530

    Update ReservationsService.java
    Added method to get the booking record

--- FlightReservationSystem/src/main/java/com/flightreservationsystem/Services/ReservationsService.java
+++ FlightReservationSystem/src/main/java/com/flightreservationsystem/Services/ReservationsService.java
@@ -1,6 +1,9 @@
 package com.flightreservationsystem.Services;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
+
 import com.flightreservationsystem.Models.Reservations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,13 @@ public class ReservationsService
     {
         List<Reservations> myReservations = iReservationsRepository.findByUsername(username);
         return myReservations;
+    }
+
+    // Getting the booking
+    public Optional<Reservations> getBookingByUsernameAndBookingDate(String username, Date bookingDate)
+    {
+        Optional<Reservations> filteredBooking = iReservationsRepository.findByUsernameAndBookingDate(username, bookingDate);
+        return filteredBooking;
     }
 
 }
